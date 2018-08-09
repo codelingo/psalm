cadabd167467bffbb4283b5efe2959c35a3d41b2
BlakeMScurr <blakemscurr@gmail.com>
2018-08-09 12:10:18 +1200

    Tidy up callgraph node.

--- src/Psalm/CallGraphNode.php
+++ src/Psalm/CallGraphNode.php
@@ -2,44 +2,32 @@
 namespace Psalm;
 
 class location {
-    public $start_line;
-    public $end_line;
     public $start_offset;
     public $end_offset;
-    public $start_column;
-    public $end_column;
 
-    public $file_path;
     public $file_name;
 
     public function __construct($internalLocation) {
-        $this->start_line = $internalLocation->getLineNumber();
-        $this->end_line = $internalLocation->getEndLineNumber();
-        $this->start_column = $internalLocation->getColumn();
-        $this->end_column = $internalLocation->getEndColumn();
-
         list($this->start_offset, $this->end_offset) = $internalLocation->getSelectionBounds();
-
-        $this->file_path = $internalLocation->file_path;
         $this->file_name = $internalLocation->file_name;
     }
 }
 
 class CallGraphNode {
-    public $definitionLocation;
-    public $referenceLocations = [];
+    public $to = []; // referenceLocations
+    public $from; // definitionLocation
 
     public function __construct($def, $refs) {
-        $this->definitionLocation = new location($def);
+        $this->from = new location($def);
         foreach ($refs as $refset) {
             foreach ($refset as $ref) {
-                $this->referenceLocations[] = new location($ref);
+                $this->to[] = new location($ref);
             }
         }
     }
 
     public function print() {
-        $json = json_encode($this, JSON_PRETTY_PRINT);
+        $json = json_encode($this, JSON_UNESCAPED_UNICODE);
         print $json . "\r\n";
     }
 }
